199639679343772941f03d2fd068d4fd3bca7395
jihyun1029 <parkji2028@naver.com>
2025-04-11 16:22:17 +0900

    Form에서 값 읽어오기

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState(0);
+    const getName = (event) => {
+        // console.log(event.target.value);
+        setName(event.target.value);
+    }
+
     return (
         <div>
             <Form>
                 <Form.Group className="mb-3" controlId="formName">
                     <Form.Label>이름</Form.Label>
-                    <Form.Control type="email" placeholder="이름을 입력해주세요" />
+                    <Form.Control
+                        type="email"
+                        placeholder="이름을 입력해주세요"
+                        // onChange={getName}
+                        onChange={(event) => setName(event.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formContact">
                     <Form.Label>전화번호</Form.Label>
-                    <Form.Control type="number" placeholder="전화번호를 입력해주세요" />
+                    <Form.Control
+                        type="number"
+                        placeholder="전화번호를 입력해주세요"
+                        onChange={(event) => setPhoneNumber(event.target.value)}
+                    />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     추가
